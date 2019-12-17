008032e80cc3480781af92c51ea24a74769a3749
zoley <zole1314@163.com>
2019-12-18 02:43:08 +0800

    rate

--- src/views/ratings/index.vue
+++ src/views/ratings/index.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="ratings-box" v-show="isShow">
+  <div class="ratings-box">
     <div class="ratings-wrap">
-      哈哈哈
+      <div class="overview z-flex">
+        <div class="overview-left z-flex">
+          <div class="score">{{seller.score}}</div>
+          <div class="desc">综合评分</div>
+          <div class="rate">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right z-flex">
+          <div class="star-wrap">
+            <label>服务态度</label>
+            <star :score="seller.serviceScore" :size="18"/>
+            <span>{{seller.serviceScore}}</span>
+          </div>
+          <div class="star-wrap">
+            <label>描述相符</label>
+            <star :score="seller.foodScore" :size="18"/>
+            <span>{{seller.foodScore}}</span>
+          </div>
+          <div class="star-wrap">
+            <label>送达时间</label>
+            <span>{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
 <script>
+import star from '@/components/star/index'
 export default {
   props:{
-    selectedFood:{
+    seller:{
       type:Object,
       default(){
         return {}
@@ -18,16 +40,17 @@ export default {
   },
   data() {
     return {
-      isShow:false
+  
     }
+  },
+  components:{
+    star
   },
   mounted() {
-    console.log(this.selectedFood);
+    console.log(this.seller);
   },
   methods: {
-    showRatings(){
-      this.isShow=true;
-    }
+
   }
 };
 </script>
